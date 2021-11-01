a49f6fc6bac3f252f55bf66216c437b84cfa9d6b
Anna Sakurai <zenasak@gmail.com>
2021-11-01 21:54:58 +0100

    first commit to pseudo

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,61 @@
-// url : https://emoji-api.com/emojis?access_key=bc60e3c945a9f4122bb298304962837ab7f070e4
+// url : https://emoji-api.com/emojis?access_key=0b8f433fa656862fd20282da5f9f0dc5be5a1946
+// key : 0b8f433fa656862fd20282da5f9f0dc5be5a1946
 
-//CREATE variable listOfEmojies
+//CREATE variable listOfEmojis
+let listOfEmojis = false;
 
-function getEmojies() {
-  /*
+function getEmojis() {
+  //FETCH emojis from api
+  fetch(
+    'https://emoji-api.com/emojis?access_key=0b8f433fa656862fd20282da5f9f0dc5be5a1946'
+  )
+    //CONVERT response from api to json
+    .then((response) => response.json())
+    .then((data) => {
+      listOfEmojis = data;
+      console.log('LIST', listOfEmojis);
+      //console.log('array?', Array.isArray(data));
 
- 
- FEATCH emojies from api 
- CONVERT responce from featch to json
- IF response from api is an array
- STORE converted res in variable listOfEmojies
- ENDIF
-
- */
+      //IF response from api is an array
+      //STORE converted res in variable listOfEmojis
+    });
 }
 
-// ACTION user presses the button
+//ACTION user presses the button
 function searchForEmoji(evt) {
-  var searchStr = new FormData(evt).get("search");
-  /*
- GET user input in searchfield
- STORE the value in a variable
- 
- IF user input is a string
- ENDIF
+  if (listOfEmojis) {
+    // GET user input in searchfield
+    // STORE the value in a variable
+    var searchStr = new FormData(evt).get('search');
 
- CLEAR #emojies element
- FOR all as ITEM in listOfEmojies
-  IF the variable from user input is the same as the ITEM description
-   STORE the item in a variable
-   PRINT the variable in #emojis by innerHTML
-  ELSE
-   PRINT "there are no matches" in #emojies
-  ENDIF
- ENDFOR
- */
+    // IF user input is a string
+    if (typeof searchStr === 'string') {
+      let emojis = document.getElementById('emojis');
+      // CLEAR #emojis element
+      emojis.innerHTML = '';
+      // FOR  all as ITEM in listOfEmojis
+      listOfEmojis.forEach((element) => {
+        // IF the variable from user input is the same as the ITEM description
+        if (
+          element.unicodeName.includes(searchStr) &&
+          !emojis.innerHTML.includes(element.character)
+        ) {
+          // STORE the item in a variable
+          emojis.innerHTML += element.character;
+        }
+      });
+
+      if (emojis.innerHTML === '') {
+        emojis.innerHTML = 'There are no matches';
+      }
+    }
+
+    // ENDIF
+  }
+
+  //      PRINT the variable in 3emojis by innerHTML
+  //    ELSE
+  //      PRINT "there are no matches" in #emojisr
+  //    ENDIF
+  // ENDFOR
 }
